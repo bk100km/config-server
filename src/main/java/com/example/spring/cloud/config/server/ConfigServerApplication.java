47b3ffc70255acec0d86953388f482bf7cb3573c
bk100km <bk100km@naver.com>
2025-09-02 09:24:59 +0900

    :recycle: 패키지명 변경

--- src/main/java/com/example/spring/cloud/config/server/ConfigServerApplication.java
+++ src/main/java/com/example/spring/cloud/config/server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.configserver;
+package com.example.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
